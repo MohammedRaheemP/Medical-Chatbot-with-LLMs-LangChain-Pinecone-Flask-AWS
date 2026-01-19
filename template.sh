7f423a6314e058a5cd767e31ebac1e22ccc7cc45
Mohammed Raheem P <raheem.reym@gmail.com>
2026-01-20 02:07:48 +0530

    folder created in structure

---
+++ template.sh
@@ -0,0 +1,16 @@
+# create directories
+mkdir -p src
+mkdir -p research
+
+#create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch src/setup.py
+touch src/app.py
+
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully."
